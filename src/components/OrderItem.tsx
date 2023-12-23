5689d4c17f432c00653ea9cf19077b6aabe9ad0f
Albert Alvarez Estelles <114157589+kukapu@users.noreply.github.com>
2023-12-23 15:07:36 +0100

    squares smaller for mobile

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -49,7 +49,7 @@ export const OrderItem = ({ color }: OrderItemProps) => {
       {...attributes}
       {...listeners}
     >
-      <div className={`flex gap-2 items-center justify-center w-20 h-20 touch-none ${getColorClass(color)}`}></div>
+      <div className={`flex gap-2 items-center justify-center w-16 h-16 touch-none ${getColorClass(color)}`}></div>
     </div>
   )
 }
